31d5a259b913b0df06876eeb4635d654b0076b82
Camila Conte <caxconte@gmail.com>
2021-09-02 11:16:14 -0300

    firebase install complete

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-const auth = getAuth();
-const db = getFirestore();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBM4nhESfR8CSq8u_PSpyoCV2lteoRuwLs",
-    authDomain: "fcbooking-8c1b2.firebaseapp.com",
-    projectId: "fcbooking-8c1b2",
-    storageBucket: "fcbooking-8c1b2.appspot.com",
-    messagingSenderId: "289034697015",
-    appId: "1:289034697015:web:91c18f504411966fdc02b2",
-    measurementId: "G-3RZ0N288WE"
+  apiKey: "AIzaSyBQ6tKQpOF7LnPpbELCWf5GxMWVq-7rAo8",
+  authDomain: "fcamara-62f52.firebaseapp.com",
+  projectId: "fcamara-62f52",
+  storageBucket: "fcamara-62f52.appspot.com",
+  messagingSenderId: "686412733569",
+  appId: "1:686412733569:web:91b71d1bc9c21a060b8180",
+  measurementId: "G-LSNNHWL4R4"
 };
 
 // Initialize Firebase
