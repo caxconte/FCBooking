3b9e853d9c323ea33e41bc4433dae881869c3898
Jean Victor <jeanvictor.lopes@gmail.com>
2021-09-05 09:33:28 -0300

    Adicionando funcionalidade
    adicionando funcionalidade em alguns botões e fazendo alguns redirecionamentos

--- src/pages/autenticacao/login.js
+++ src/pages/autenticacao/login.js
@@ -2,7 +2,7 @@
 const enviar = document.querySelector('#enviarAutenticacao');
 
 //verifica o click no submit
-enviar.addEventListener('click', () => {  
+enviar.addEventListener('click', () => {
     //pega o input de nome
     const nome = document.querySelector('#nomeAutenticacao');
     const valorNome = nome.value;
@@ -12,5 +12,10 @@ enviar.addEventListener('click', () => {
     const email = document.querySelector('#emailAutenticacao');
     const valorEmail = email.value;
     console.log(valorEmail);
+
+    //redirecionamento
+    if (valorNome != "" && valorEmail != "") {
+        location.href = '../booking/booking.html';
+    }
 });
 
