701eb3ca6e5c9563efcc5b5cd341d05216d9418e
Beatriz Vieira <55674401+vbribeiro@users.noreply.github.com>
2021-09-09 00:30:32 -0300

    nova tela de login estilizada

---
+++ login/login.js
@@ -0,0 +1,15 @@
+//pega o input submit
+const enviar = document.querySelector('#enviarAutenticacao');
+
+//verifica o click no submit
+enviar.addEventListener('click', () => {  
+    //pega o input de nome
+    const nome = document.querySelector('.inputNome');
+    const valorNome = nome.value;
+    console.log(valorNome);
+
+    //pega o input de email
+    const email = document.querySelector('#emailAutenticacao');
+    const valorEmail = email.value;
+    console.log(valorEmail);
+});
